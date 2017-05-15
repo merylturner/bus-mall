bac5afe8cb35e9e42229210ef7afdb9992088087
Meryl Turner <merylturner08@gmail.com>
2017-05-15 14:39:38 -0700

    added constructor function and instances

---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+//          DATA            //
+var allItems = [];
+
+//          CONSTRUCTOR FUNCTION & INSTANCES            //
+
+function Item (name, filepath, numShown, numClicked, id) {
+    this.name = name;
+    this.filepath = filepath;
+    this.numShown = numShown;
+    this.numClicked = numClicked;
+    this.id = id;
+}
+
+var bag = new Item ('bag', 'imgs/bag.jpg', );
+var banana = new Item ('banana', 'imgs/banana.jpg', );
+var bathroom = new Item ('bathroom', 'imgs/bathroom.jpg', );
+var boots = new Item ('boots', 'imgs/boots.jpg', );
+var breakfast = new Item ('breakfast', 'imgs/breakfast.jpg', );
+var bubblegum = new Item ('bubblegum', 'imgs/bubblegum.jpg', );
+var chair = new Item ('chair', 'imgs/chair.jpg', );
+var cthulhu = new Item ('cthulhu', 'imgs/cthulhu.jpg', );
+var dogDuck = new Item ('dog-duck', 'imgs/dog-duck.jpg', );
+var dragon = new Item ('dragon', 'imgs/dragon.jpg', );
+var pen = new Item ('pen', 'imgs/pen.jpg', );
+var petSweep = new Item ('pet-sweep', 'imgs/pet-sweep.jpg', );
+var scissors = new Item ('scissors', 'imgs/scissors.jpg', );
+var shark = new Item ('shark', 'imgs/shark.jpg', );
+var sweep = new Item ('sweep', 'imgs/sweep.jpg', );
+var tauntaun = new Item ('tauntaun', 'imgs/tauntaun.jpg', );
+var unicorn = new Item ('unicorn', 'imgs/unicorn.jpg', );
+var usb = new Item ('usb', 'imgs/usb.gif', );
+var waterCan = new Item ('water-can', 'imgs/water-can.jpg', );
+var wineGlass = new Item ('wine-glass', 'imgs/wine-glass.jpg', );
+
+
+
+
+
+
+
+//          FUNCTION ____            //
