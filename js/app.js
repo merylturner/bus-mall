14217ce79f67459afe3a1287ef4d9055feda0a82
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-05-15 17:38:53 -0700

    created array of random numbers TODO for loop checking against duplicates

--- js/app.js
+++ js/app.js
@@ -22,19 +22,19 @@ var boots = new Item('boots', 'boots.jpg', 0, 0, 'boots');
 var breakfast = new Item('breakfast', 'breakfast.jpg', 0, 0, 'breakfast');
 var bubblegum = new Item('bubblegum', 'bubblegum.jpg', 0, 0, 'bubblegum');
 var chair = new Item('chair', 'chair.jpg', 0, 0, 'chair');
-// var cthulhu = new Item('cthulhu', 'cthulhu.jpg', 0, 0, 'cthulhu');
-// var dogDuck = new Item('dog duck', 'dog-duck.jpg', 0, 0, 'dog-duck');
-// var dragon = new Item('dragon', 'dragon.jpg', 0, 0, 'dragon');
-// var pen = new Item('pen', 'pen.jpg', 0, 0, 'pen');
-// var petSweep = new Item('pet sweep', 'pet-sweep.jpg', 0, 0, 'pet-sweep');
-// var scissors = new Item('scissors', 'scissors.jpg', 0, 0, 'scissors');
-// var shark = new Item('shark', 'shark.jpg', 0, 0, 'shark');
-// var sweep = new Item('sweep', 'sweep.jpg', 0, 0, 'sweep');
-// var tauntaun = new Item('tauntaun', 'tauntaun.jpg', 0, 0, 'tauntaun');
-// var unicorn = new Item('unicorn', 'unicorn.jpg', 0, 0, 'unicorn');
-// var usb = new Item('usb', 'usb.gif', 0, 0, 'usb');
-// var waterCan = new Item('water can', 'water-can.jpg', 0, 0, 'water-can');
-// var wineGlass = new Item('wine glass', 'wine-glass.jpg', 0, 0, 'wine-glass');
+var cthulhu = new Item('cthulhu', 'cthulhu.jpg', 0, 0, 'cthulhu');
+var dogDuck = new Item('dog duck', 'dog-duck.jpg', 0, 0, 'dog-duck');
+var dragon = new Item('dragon', 'dragon.jpg', 0, 0, 'dragon');
+var pen = new Item('pen', 'pen.jpg', 0, 0, 'pen');
+var petSweep = new Item('pet sweep', 'pet-sweep.jpg', 0, 0, 'pet-sweep');
+var scissors = new Item('scissors', 'scissors.jpg', 0, 0, 'scissors');
+var shark = new Item('shark', 'shark.jpg', 0, 0, 'shark');
+var sweep = new Item('sweep', 'sweep.png', 0, 0, 'sweep');
+var tauntaun = new Item('tauntaun', 'tauntaun.jpg', 0, 0, 'tauntaun');
+var unicorn = new Item('unicorn', 'unicorn.jpg', 0, 0, 'unicorn');
+var usb = new Item('usb', 'usb.gif', 0, 0, 'usb');
+var waterCan = new Item('water can', 'water-can.jpg', 0, 0, 'water-can');
+var wineGlass = new Item('wine glass', 'wine-glass.jpg', 0, 0, 'wine-glass');
 
 
 
@@ -47,39 +47,54 @@ var tracker = {
     displaySection: document.getElementById('display'),
     votes: 0,
 
-// get random number within allItems array.length
+    // get random number within allItems array.length
     randomIndex: function (arr) {
         return Math.floor(Math.random() * allItems.length);
     },
 
 
-// get random index and push to selected index array
+    // get random index and push to selected index array
     getIndex: function (arr) {
         var selectedIndex = [];
     },
 
 
-//display three options of products, using properties of tracker object
+    //display three options of products, using properties of tracker object
 
     displayOptions: function () {
         //get 3 random items
-        var rand1 = this.randomIndex();
-        var rand2 = this.randomIndex();
-        var rand3 = this.randomIndex();
-        var theseDoMatch = rand1 === rand2 || rand1 === rand3 || rand2 === rand3;
+        var randNum = [this.randomIndex(), this.randomIndex(), this.randomIndex()];
+    
+        //conditional if pictures are matching
+        var theseDoMatch = randNum[0] === randNum[1] || randNum[0] === randNum[2] || randNum[1] === randNum[2];
 
         while (theseDoMatch) {
             //reassign value to variables
-            rand1 = this.randomIndex();
-            rand2 = this.randomIndex();
-            rand3 = this.randomIndex();
+            randNum[0] = this.randomIndex();
+            randNum[1]= this.randomIndex();
+            randNum[2] = this.randomIndex();
+            //rerun 
+            theseDoMatch = randNum[0] === randNum[1] || randNum[0] === randNum[2] || randNum[1] === randNum[2];
+        };
+
+        var item1 = allItems[randNum[0]];
+        var item2 = allItems[randNum[1]];
+        var item3 = allItems[randNum[2]];
+
+        console.log(allItems);
+
+        //create array of firstRound results
+        this.firstRound = [randNum[0], randNum[1], randNum[2]];
+
+
+        //run for loop through firstRound array to check against randNum array
+        for (var i = 0; i < this.firstRound.length; i++) {
+            if (randNum[i] !== this.firstRound[i]) {
+                return randNum[i];
+            }
+
         }
 
-        var item1 = allItems[rand1];
-        var item2 = allItems[rand2];
-        var item3 = allItems[rand3];
-        console.log(item1);
-        console.log(allItems);
 
         // append items to DOM
         this.option1.src = item1.filepath;
@@ -89,24 +104,24 @@ var tracker = {
 
     },
 
-    // tallyVote: function (id) {
-    //     this.votes += 1;
+    tallyVote: function (id) {
+        this.votes += 1;
 
-    //     for (var i = 0; i < allItems.length; i++) {
-    //         var item = allItems[i];
-    //         if (item.id === id) {
-    //             item.votes += 1;
-    //         }
-    //     }
-    // },
+        for (var i = 0; i < allItems.length; i++) {
+            var item = allItems[i];
+            if (item.id === id) {
+                item.votes += 1;
+            }
+        }
+    },
 
-    // showResults: function () {
-    //     this.displaySection.removeEventListener('click', voteHandler);
-    //     for (var i = 0; i < allItems.length; i++) {
-    //         var item = allItems[i];
-    //         console.log(item.name + ': ' + item.votes);
-    //     }
-    // }
+    showResults: function () {
+        this.displaySection.removeEventListener('click', voteHandler);
+        for (var i = 0; i < 26; i++) {
+            var item = allItems[i];
+            console.log(item.name + ': ' + item.votes);
+        }
+    }
 
 }
 
